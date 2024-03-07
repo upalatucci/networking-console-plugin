cf79067ac1a4e3eebe13efcbad3266cc0d0b0195
Ugo Palatucci <upalatuc@redhat.com>
2024-03-07 18:22:18 +0100

    fixing confirm modal

--- src/utils/components/ConfirmModal/ConfirmModal.tsx
+++ src/utils/components/ConfirmModal/ConfirmModal.tsx
@@ -1,27 +1,60 @@
 import React, { FC } from 'react';
 
-import { Modal, ModalVariant } from '@patternfly/react-core';
+import {
+  ActionList,
+  ActionListItem,
+  Button,
+  ButtonVariant,
+  Modal,
+  ModalVariant,
+} from '@patternfly/react-core';
+
+import './ConfirmModal.scss';
+import { useNetworkingTranslation } from '@utils/hooks/useNetworkingTranslation';
 
 export type ConfirmModalProps = {
   executeFn: () => void;
   title: string;
   message: string | JSX.Element;
   btnText: string | JSX.Element;
-  close?: () => void;
+  closeModal?: () => void;
 };
 
-const ConfirmModal: FC<ConfirmModalProps> = ({ title, children }) => {
+const ConfirmModal: FC<ConfirmModalProps> = ({
+  title,
+  closeModal,
+  executeFn,
+  btnText,
+  message,
+}) => {
+  const { t } = useNetworkingTranslation();
+
   return (
     <Modal
       className="ocs-modal networking-confirm-modal"
       id="confirm-modal"
-      isOpen={true}
-      onClose={close}
+      isOpen
+      onClose={closeModal}
       position={'top'}
       title={title}
       variant={ModalVariant.small}
+      titleIconVariant="warning"
+      footer={
+        <ActionList className="tabmodal-footer">
+          <ActionListItem>
+            <Button onClick={executeFn} variant={ButtonVariant.primary}>
+              {btnText || t('Confirm')}
+            </Button>
+          </ActionListItem>
+          <ActionListItem>
+            <Button onClick={closeModal} variant="link">
+              {t('Cancel')}
+            </Button>
+          </ActionListItem>
+        </ActionList>
+      }
     >
-      {children}
+      {message}
     </Modal>
   );
 };
