7ef54e8c23dac91dc1b6c19c1d3e9fd67a1c8931
Matan Schatzman <mschatzm@redhat.com>
2024-04-15 09:30:15 +0300

    remove alias

--- webpack.config.ts
+++ webpack.config.ts
@@ -116,12 +116,6 @@ const config: Configuration = {
     }),
   ],
   resolve: {
-    alias: {
-      '@console/internal': path.join(
-        __dirname,
-        './node_modules/@openshift-console/dynamic-plugin-sdk/lib',
-      ),
-    },
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     plugins: [new TsconfigPathsPlugin()],
   },
