c081c48744cb08d26691229af1d62c07efef498c
Aviv Turgeman <aturgema@redhat.com>
2024-09-22 17:20:23 +0300

    Revert IPAM (subnets input)
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/views/nads/form/components/NetworkTypeParameters/utils/constants.ts
+++ src/views/nads/form/components/NetworkTypeParameters/utils/constants.ts
@@ -3,13 +3,13 @@ import { FC } from 'react';
 import { NetworkTypeKeys, NetworkTypeKeysType } from '@views/nads/form/utils/types';
 
 import BridgeParameters from '../components/BridgeParameters/BridgeParameters';
-import Layer2Parameters from '../components/Layer2Parameters/Layer2Parameters';
+// import Layer2Parameters from '../components/Layer2Parameters/Layer2Parameters';
 import SecondaryLocalnetParameters from '../components/OVNK8sSecondaryLocalnetParameters/OVNK8sSecondaryLocalnetParameters';
 import SriovParameters from '../components/SriovParameters/SriovParameters';
 
 export const networkTypeComponentMapper: Record<NetworkTypeKeysType, FC> = {
   [NetworkTypeKeys.cnvBridgeNetworkType]: BridgeParameters,
-  [NetworkTypeKeys.ovnKubernetesNetworkType]: Layer2Parameters,
+  [NetworkTypeKeys.ovnKubernetesNetworkType]: null, //Layer2Parameters revert subnets to 4.18
   [NetworkTypeKeys.ovnKubernetesSecondaryLocalnet]: SecondaryLocalnetParameters,
   [NetworkTypeKeys.sriovNetworkType]: SriovParameters,
 };
