17b28df33bdccfa45ae4d0f0765cd637b8997e7e
Ugo Palatucci <upalatuc@redhat.com>
2025-01-24 12:13:01 +0100

    CNV-55234: do not show all namesapces in cudn namespace column

--- src/views/udns/list/components/UserDefinedNetworkRow.tsx
+++ src/views/udns/list/components/UserDefinedNetworkRow.tsx
@@ -13,6 +13,7 @@ import { UserDefinedNetworkModel } from '@utils/models';
 import { getName, getNamespace } from '@utils/resources/shared';
 import { getModel, getTopology } from '@utils/resources/udns/selectors';
 import { ClusterUserDefinedNetworkKind, UserDefinedNetworkKind } from '@utils/resources/udns/types';
+import { NO_DATA_DASH } from '@utils/utils/constants';
 import UDNActions from '@views/udns/actions/UDNActions';
 
 type UserDefinedNetworkRowType = RowProps<ClusterUserDefinedNetworkKind | UserDefinedNetworkKind>;
@@ -40,7 +41,7 @@ const UserDefinedNetworkRow: FC<UserDefinedNetworkRowType> = ({ activeColumnIDs,
             name={namespace}
           />
         ) : (
-          t('All namespaces')
+          NO_DATA_DASH
         )}
       </TableData>
       <TableData activeColumnIDs={activeColumnIDs} id="topology">
