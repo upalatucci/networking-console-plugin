401bde240c59d4280708bec065030acc79da2fb5
Ugo Palatucci <upalatuc@redhat.com>
2025-02-03 10:02:45 +0100

    CNV-55744: fix nads list crash

--- src/utils/resources/nads/helpers.ts
+++ src/utils/resources/nads/helpers.ts
@@ -2,4 +2,4 @@ import { UDN_LABEL } from './constants';
 import { NetworkAttachmentDefinitionKind } from './types';
 
 export const isUserDefinedNetworkNAD = (nad: NetworkAttachmentDefinitionKind) =>
-  Object.keys(nad?.metadata?.labels).includes(UDN_LABEL);
+  Object.keys(nad?.metadata?.labels || {})?.includes(UDN_LABEL);
