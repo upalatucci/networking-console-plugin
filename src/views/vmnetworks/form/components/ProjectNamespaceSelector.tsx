c06fb823b41c7220f9c8ba8a38d4445a7d18df44
Ugo Palatucci <upalatuc@redhat.com>
2025-08-11 11:23:52 +0200

    CNV-52160: add awareness checkbox

--- src/views/vmnetworks/form/components/ProjectNamespaceSelector.tsx
+++ src/views/vmnetworks/form/components/ProjectNamespaceSelector.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 
-import { Button, ButtonVariant, FormSection } from '@patternfly/react-core';
+import { Button, ButtonVariant, Checkbox, FormSection } from '@patternfly/react-core';
 import { PlusCircleIcon } from '@patternfly/react-icons';
 import LabelSelectorEditor from '@utils/components/LabelSelectorEditor/LabelSelectorEditor';
 import { useNetworkingTranslation } from '@utils/hooks/useNetworkingTranslation';
@@ -30,6 +30,20 @@ const ProjectNamespaceSelector: FC = () => {
               >
                 {t('Add a label to specify qualifying projects')}
               </Button>
+              <Controller
+                control={control}
+                name="matchLabelCheck"
+                render={({ field: { onChange, value } }) => (
+                  <Checkbox
+                    isChecked={value}
+                    id="check-empty-matchlabel"
+                    label={t(
+                      "I'm aware that if no label is specified, all current and future projects will have access to this network. To prevent this, add labels to the network",
+                    )}
+                    onChange={(_, checked) => onChange(checked)}
+                  />
+                )}
+              />
             </div>
           ) : (
             <LabelSelectorEditor
